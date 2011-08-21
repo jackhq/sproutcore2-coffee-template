0af727419335ea96053c277efb94bbf05ead28c0
Tom <team+tom@jackrussellsoftware.com@jackrussellsoftware.com>
2011-08-21 13:40:08 -0400

    update friends and cats

--- play_app.coffee
+++ play_app.coffee
@@ -1,4 +1,10 @@
+cats = ['best','coworker','basic']
+
 Play = SC.Application.create()
+
+Play.Category = SC.Object.extend
+  name: null
+  friends: null
 
 Play.Friend = SC.Object.extend
   name: null
@@ -8,6 +14,17 @@ Play.Friend = SC.Object.extend
   morning: false
   noon: false
 
+Play.categoriesController = SC.ArrayProxy.create
+  content: []
+  arrayDidChange: (item, idx, removedCnt, addedCnt) ->
+    @_super(item, idx, removedCnt, addedCnt)
+    console.log item
+    console.log idx
+    if item? and item.length > 0 and item[idx].friends is null
+      item[idx].friends = Play.friendsController.filterProperty('category', 'best') if item[idx].name is 'best'
+      item[idx].friends = Play.friendsController.filterProperty('category', 'coworker') if item[idx].name is 'coworker'
+      item[idx].friends = Play.friendsController.filterProperty('category', 'basic')  if item[idx].name is 'basic'
+      console.log 'Added Friends...'
 
 Play.friendsController = SC.ArrayProxy.create
   content: []
@@ -19,10 +36,11 @@ Play.friendsController = SC.ArrayProxy.create
     @set 'best', @filterProperty('category', 'best')
     @set 'coworkers', @filterProperty('category', 'coworker')
     @set 'basic', @filterProperty('category', 'basic')
+  createFriend: ->
+    @pushObject(Play.Friend.create())
 
 
 window.Play = Play
-
 
 #store = []
 # Create Friends
@@ -35,6 +53,8 @@ Play.friendsController.pushObject(Play.Friend.create(name: friend.name, category
   { name: 'Sue', category: 'coworker' }
 
 ]
+
+Play.categoriesController.pushObject(Play.Category.create(name: cat)) for cat in cats
 
 #Play.friendsController.set('content', store)
 
--- play_app.js
+++ play_app.js
@@ -1,6 +1,11 @@
 (function() {
-  var Play, friend, _i, _len, _ref;
+  var Play, cat, cats, friend, _i, _j, _len, _len2, _ref;
+  cats = ['best', 'coworker', 'basic'];
   Play = SC.Application.create();
+  Play.Category = SC.Object.extend({
+    name: null,
+    friends: null
+  });
   Play.Friend = SC.Object.extend({
     name: null,
     category: null,
@@ -8,6 +13,26 @@
     early_morning: false,
     morning: false,
     noon: false
+  });
+  Play.categoriesController = SC.ArrayProxy.create({
+    content: [],
+    arrayDidChange: function(item, idx, removedCnt, addedCnt) {
+      this._super(item, idx, removedCnt, addedCnt);
+      console.log(item);
+      console.log(idx);
+      if ((item != null) && item.length > 0 && item[idx].friends === null) {
+        if (item[idx].name === 'best') {
+          item[idx].friends = Play.friendsController.filterProperty('category', 'best');
+        }
+        if (item[idx].name === 'coworker') {
+          item[idx].friends = Play.friendsController.filterProperty('category', 'coworker');
+        }
+        if (item[idx].name === 'basic') {
+          item[idx].friends = Play.friendsController.filterProperty('category', 'basic');
+        }
+        return console.log('Added Friends...');
+      }
+    }
   });
   Play.friendsController = SC.ArrayProxy.create({
     content: [],
@@ -19,6 +44,9 @@
       this.set('best', this.filterProperty('category', 'best'));
       this.set('coworkers', this.filterProperty('category', 'coworker'));
       return this.set('basic', this.filterProperty('category', 'basic'));
+    },
+    createFriend: function() {
+      return this.pushObject(Play.Friend.create());
     }
   });
   window.Play = Play;
@@ -48,6 +76,12 @@
     Play.friendsController.pushObject(Play.Friend.create({
       name: friend.name,
       category: friend.category
+    }));
+  }
+  for (_j = 0, _len2 = cats.length; _j < _len2; _j++) {
+    cat = cats[_j];
+    Play.categoriesController.pushObject(Play.Category.create({
+      name: cat
     }));
   }
 }).call(this);
